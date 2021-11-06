dba7e608682aca63b0ff71696db0187c38f31522
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-11-06 20:34:09 +0700

    menambahkan handler function di main.go

--- main.go
+++ main.go
@@ -10,21 +10,22 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Bayu Bagus Bagaswara",
-			"bio":  "A Software engineer & content creator",
-		})
-	})
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"title":    "Hello World",
-			"subtitle": "Belajar Golang",
-		})
-	})
-
-	// kita bisa mengubah port untuk localhost
 	router.Run(":8081")
+}
 
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Bayu Bagus Bagaswara",
+		"bio":  "A Software engineer & content creator",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":    "Hello World",
+		"subtitle": "Belajar Golang",
+	})
 }
