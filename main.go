b3306e1f921256ef01fa70bc466300658ef8cf8a
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-11-07 09:20:06 +0700

    Path Parameter dan Query String

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ func main() {
 
 	router.GET("/", rootHandler)
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id", booksHandler)
+	router.GET("/query", queryHandler)
 
 	router.Run(":8081")
 }
@@ -27,5 +29,28 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"title":    "Hello World",
 		"subtitle": "Belajar Golang",
+	})
+}
+
+// Path Parameter
+func booksHandler(c *gin.Context) {
+	// tangkap id nya menggunakan Param
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+// Query String
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"price": price,
 	})
 }
