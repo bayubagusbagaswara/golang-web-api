59a7dae476b0b6c01aa2f0b46ed46656ad6e0c09
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-11-08 10:26:41 +0700

    menampilkan pesan error validasi

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -56,9 +58,8 @@ func queryHandler(c *gin.Context) {
 }
 
 type BookInput struct {
-	// tambahkan validation required artinya wajib diisi
-	Title string `json:"title" binding:"required"`
-	Price int    `json:"price" binding:"required, number"`
+	Title string      `json:"title" binding:"required"`
+	Price json.Number `json:"price" binding:"required, number"`
 }
 
 func postBooksHandler(c *gin.Context) {
@@ -66,11 +67,14 @@ func postBooksHandler(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&bookInput)
 	if err != nil {
-		// jika ada error, maka kita kasis status http bad request
-		// error diakibatkan dari gagal validasi
-		c.JSON(http.StatusBadRequest, err)
-		fmt.Println(err)
-		return
+
+		for _, e := range err.(validator.ValidationErrors) {
+
+			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
+			c.JSON(http.StatusBadRequest, errorMessage)
+			return
+
+		}
 	}
 
 	c.JSON(http.StatusOK, gin.H{
