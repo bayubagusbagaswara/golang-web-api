1c794e3bf2cf89c44462dba162e12ed5a8c2f08f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-11-09 18:33:26 +0700

    Mengubah Struktur Project

--- main.go
+++ main.go
@@ -1,92 +1,23 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
+	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 func main() {
 
 	router := gin.Default()
 
-	// buat variable untuk versioning
 	v1 := router.Group("/v1")
 
-	v1.GET("/", rootHandler)
-	v1.GET("/hello", helloHandler)
-	v1.GET("/books/:id", booksHandler)
-	v1.GET("/query", queryHandler)
+	v1.GET("/", handler.RootHandler)
+	v1.GET("/hello", handler.HelloHandler)
+	v1.GET("/books/:id", handler.BooksHandler)
+	v1.GET("/query", handler.QueryHandler)
 
-	v1.POST("/books", postBooksHandler)
+	v1.POST("/books", handler.PostBooksHandler)
 
 	router.Run(":8081")
-}
-
-func rootHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"name": "Bayu Bagus Bagaswara",
-		"bio":  "A Software engineer & content creator",
-	})
-}
-
-func helloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"title":    "Hello World",
-		"subtitle": "Belajar Golang",
-	})
-}
-
-func booksHandler(c *gin.Context) {
-	id := c.Param("id")
-	title := c.Param("title")
-
-	c.JSON(http.StatusOK, gin.H{
-		"id":    id,
-		"title": title,
-	})
-}
-
-func queryHandler(c *gin.Context) {
-	title := c.Query("title")
-	price := c.Query("price")
-
-	c.JSON(http.StatusOK, gin.H{
-		"title": title,
-		"price": price,
-	})
-}
-
-type BookInput struct {
-	Title string      `json:"title" binding:"required"`
-	Price json.Number `json:"price" binding:"required, number"`
-}
-
-func postBooksHandler(c *gin.Context) {
-	var bookInput BookInput
-
-	err := c.ShouldBindJSON(&bookInput)
-	if err != nil {
-
-		errorMessages := []string{}
-		for _, e := range err.(validator.ValidationErrors) {
-
-			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
-
-			errorMessages = append(errorMessages, errorMessage)
-		}
-		c.JSON(http.StatusBadRequest, gin.H{
-			"errors": errorMessages,
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"title": bookInput.Title,
-		"price": bookInput.Price,
-	})
-
 }
