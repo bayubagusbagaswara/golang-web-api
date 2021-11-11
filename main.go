2a71b1be517eed5a9aff22771dcfd6565144c0f0
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-11-11 14:25:42 +0700

    Menyimpan Data ke Database atau Create

--- main.go
+++ main.go
@@ -15,28 +15,36 @@ import (
 func main() {
 
 	dsn := "host=localhost user=pustakaapi password=pustakaapi dbname=pustakaapi port=5432 sslmode=disable TimeZone=Asia/Jakarta"
-
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		log.Fatal("Db connection error")
 	}
 
-	fmt.Println("Database connection succeed")
-
-	// kita akan lakukan auto migration dari data struct yang kemudian akan ditransfer ke database
-	// book adalah nama filenya, dan Book adalah nama struct di entity.go
 	db.AutoMigrate(book.Book{})
 
-	router := gin.Default()
+	// kita akan melakukan operasi CRUD untuk memanipulasi data
+	// kita bikin object dari struct book
+	book := book.Book{}
+	// isi property pada struct book
+	book.Title = "Man Tiger"
+	book.Price = 9000
+	book.Discount = 10
+	book.Rating = 5
+	book.Description = "Ini adalah yang sangat bagus dari Eka Kurniawan"
 
+	// lalu kita simpan data struct diatas, dan balikannya adalah error, yang nantinya dicek apakah ada error atau tidak
+	err = db.Create(&book).Error
+	if err != nil {
+		fmt.Println("Error creating book record")
+	}
+
+	router := gin.Default()
 	v1 := router.Group("/v1")
 
 	v1.GET("/", handler.RootHandler)
 	v1.GET("/hello", handler.HelloHandler)
 	v1.GET("/books/:id", handler.BooksHandler)
 	v1.GET("/query", handler.QueryHandler)
-
 	v1.POST("/books", handler.PostBooksHandler)
 
 	router.Run()
