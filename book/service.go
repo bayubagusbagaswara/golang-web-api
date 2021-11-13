6c388ac2afe08b4bb83e71c1313e9ceb63109fc2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-11-13 18:38:53 +0700

    Service Layer

---
+++ book/service.go
@@ -0,0 +1,43 @@
+package book
+
+// buat Service Layer
+// buat interface dulu, lalu bikin implementasinya
+// dan service ini membutuhkan layer repository
+
+type Service interface {
+	FindAll() ([]Book, error)
+	FindByID(ID int) (Book, error)
+	Create(book BookRequest) (Book, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+// parameternya adalah interface repository
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) FindAll() ([]Book, error) {
+	// return s.repository.FindAll()
+	// atau bisa tulis manual untuk menangkap errornya
+	books, err := s.repository.FindAll()
+	return books, err
+}
+
+func (s *service) FindByID(ID int) (Book, error) {
+	book, err := s.repository.FindById(ID)
+	return book, err
+}
+
+func (s *service) Create(bookRequest BookRequest) (Book, error) {
+	// kita mapping dari bookRequest menjadi book
+	price, err := bookRequest.Price.Int64()
+	book := Book{
+		Title: bookRequest.Title,
+		Price: int(price),
+	}
+	newBook, err := s.repository.Create(book)
+	return newBook, err
+}
--- main.go
+++ main.go
@@ -19,36 +19,16 @@ func main() {
 		log.Fatal("Db connection error")
 	}
 
-	// kita tidak akan langsung berhubungan dengan database
-	// tapi kita akan buat Repository Layer untuk berkomunikasi dengan database
-
 	db.AutoMigrate(book.Book{})
 
-	// kita parsing object db nya
 	bookRepository := book.NewRepository(db)
+	bookService := book.NewService(bookRepository)
 
-	// kita panggil function yang ada di repository
-	// books, err := bookRepository.FindAll()
-	// kita buktikan bahwa books ada isi datanya
-	// for _, book := range books {
-	// 	fmt.Println("Title: ", book.Title)
-	// }
-
-	// function findById
-	// book, err := bookRepository.FindById(1)
-	// fmt.Println("Title :", book.Title)
-
-	// function Create
-	// buat dulu data untuk struct book
-	book := book.Book{
-		Title:       "Laskar Pelangi",
-		Description: "Buku anak petualangan",
-		Price:       95000,
-		Rating:      4,
-		Discount:    0,
+	bookRequest := book.BookRequest{
+		Title: "Laskar Pelangi",
+		Price: "95000",
 	}
-	// lalu save
-	bookRepository.Create(book)
+	bookService.Create(bookRequest)
 
 	router := gin.Default()
 	v1 := router.Group("/v1")
